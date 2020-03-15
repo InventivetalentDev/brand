2721351fb70cce96ef440b7df64e30332fa76c7c
inventivetalent <mail@inventivetalent.org>
2020-03-15 14:10:30 +0100

    add mcassets

---
+++ brands/mcassets.js
@@ -0,0 +1,11 @@
+module.exports = {
+    url: "mcasset.cloud",
+    images: [
+        {
+            name: "Logo",
+            file: "mcassets",
+            padding: 20,
+            background: 'light'
+        }
+    ]
+};
